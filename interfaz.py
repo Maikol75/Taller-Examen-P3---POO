07d476124d443a6c84162b84b1795b539139f3eb
Maikol Asdrubal Rodriguez Ovalle <maikolasdrubal75@gmail.com>
2025-11-06 09:46:25 -0500

    interfaz.py

--- interfaz.py
+++ interfaz.py
@@ -7,42 +7,42 @@ class Interfaz:
 
     def menu(self):
         while True:
-            print("\nMenu")
-            print("1. Agregar pollos")
+            print("\nMENU PRINCIPAL")
+            print("1. Agregar pollo")
             print("2. Mostrar pollos")
             print("3. Registrar produccion de huevos")
             print("4. Ver produccion de huevos")
             print("5. Eliminar pollo")
             print("6. Actualizar pollo")
             print("7. Salir")
-            opcion = input("elije una opcion: ")
+            opcion = input("Elija una opcion: ")
 
             if opcion == '1':
-                codigo = input("Ingrese el codigo del pollo: ")
-                raza = input("Ingrese la raza del pollo: ")
-                edad = input("Ingrese la edad del pollo (años): ")
+                codigo = input("Codigo del pollo: ")
+                raza = input("Raza del pollo: ")
+                edad = input("Edad del pollo (anios): ")
                 pollo = Animal(codigo, raza, edad)
                 self.bd.agregarPollo(pollo)
 
             elif opcion == '2':
                 self.bd.mostrarPollo()
             elif opcion == '3':
-                codigo = input("Ingrese el codigo del pollo: ")
+                codigo = input("Codigo del pollo: ")
                 self.bd.registrarHuevos(codigo)
             elif opcion == '4':
-                codigo = input("Ingrese el codigo del pollo: ")
+                codigo = input("Codigo del pollo: ")
                 self.bd.mostrarProduccion(codigo)
             elif opcion == '5':
-                codigo = input("Ingrese el codigo del pollo que vas a eliminar: ")
+                codigo = input("Codigo del pollo a eliminar: ")
                 self.bd.eliminarPollo(codigo)
             elif opcion == '6':
-                codigo = input("Ingrese el código del pollo a actualizar: ")
-                self.bd.actualizarPollo(codigo)    
+                codigo = input("Codigo del pollo a actualizar: ")
+                self.bd.actualizarPollo(codigo)
             elif opcion == '7':
-                print("Saliendo del programa")
+                print("Saliendo del programa...")
                 break
             else:
-                print("Opcion invalida intente de nuevo")
+                print("opcion invalida, intente de nuevo")
 
 if __name__ == "__main__":
     app = Interfaz()
